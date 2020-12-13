472bd30439d461c335b56bb43f4835312afbb60e
Alison Major <alison.major@gmail.com>
2020-12-13 09:15:36 -0500

    Reimplement menu options

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from create import create_new_contact
 from formatters import clear_terminal_screen
 from initialize import initialize_base
-from search import search_exact_name
+from search import search_exact_name, search_partial_name, search_phone_area_code, search_phone_prefix, search_age_range
 
 MENU = '''
 ===================================
@@ -33,18 +34,18 @@ def main():
     while True:
         if menu_choice == "1":
             search_exact_name()
-    #     elif menu_choice == "2":
-    #         search_partial_name()
-    #     elif menu_choice == "3":
-    #         search_phone_area_code()
-    #     elif menu_choice == "4":
-    #         search_phone_prefix()
-    #     elif menu_choice == "5":
-    #         search_age_range()
-    #     elif menu_choice == "6":
-    #         create_new_contact()
-    #     elif menu_choice == "0":
-    #         quit()
+        elif menu_choice == "2":
+            search_partial_name()
+        elif menu_choice == "3":
+            search_phone_area_code()
+        elif menu_choice == "4":
+            search_phone_prefix()
+        elif menu_choice == "5":
+            search_age_range()
+        elif menu_choice == "6":
+            create_new_contact()
+        elif menu_choice == "0":
+            quit()
         else:
             print("\n*** Not a valid choice. Please choose from the menu. ***\n\n")
 
